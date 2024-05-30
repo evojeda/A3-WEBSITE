3852e7189ea4c06dddc54e3a14af9e561dcbada0
Evellyn <evellyn_2701@hotmail.com>
2024-05-30 15:13:19 -0300

    Criando um site

---
+++ pages.js
@@ -0,0 +1,24 @@
+
+// Header
+function menuShow() {
+    const menuList = document.querySelector('.menu-list');
+    const menuIcon = document.querySelector('.menu-icon');
+
+    menuList.classList.toggle('active');
+
+    if (menuList.classList.contains('active')) {
+        menuIcon.src = "./img/close.svg";
+        menuList.style.display = 'flex'; 
+        setTimeout(() => {
+            menuList.style.opacity = '1';
+            menuList.style.transform = 'translateY(0)';
+        }, 10); 
+    } else {
+        menuIcon.src = "./img/bars-solid.svg";
+        menuList.style.opacity = '0';
+        menuList.style.transform = 'translateY(-10px)';
+        setTimeout(() => {
+            menuList.style.display = 'none'; 
+        }, 500); 
+    }
+}
